8db31f739deac80551e86dd5cc41b8d58c2df02e
Vineet Kumar <vk.vempire@gmail.com>
2021-07-27 20:07:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './image/mlsa.png';
 import './App.css';
 
 function App() {
@@ -6,20 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+        Be a force for good
+        </h1>
+
       </header>
     </div>
   );
 }
 
 export default App;
+//npx create-react-app mlsa
